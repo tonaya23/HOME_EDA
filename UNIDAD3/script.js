80f2ae1ceaa12cd84d794749fe1a0b33fb67680d
Gerardo <angel.orozco.gzl@gmail.com>
2023-11-22 08:49:23 -0600

    Botones extras y validaciones

--- UNIDAD3/script.js
+++ UNIDAD3/script.js
@@ -1,6 +1,13 @@
-let ultimasFilasAgregadas = []; // Variable para almacenar las últimas filas agregadas
+let ultimasFilasAgregadas = [];
 
 function agregarNombre(nombreNuevo, idTabla, posicion) {
+  const regex = /^[0-9!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]+$/;
+
+  if (nombreNuevo === '' || !regex.test(nombreNuevo)) {
+    alert('Ingresa un nombre válido (números y símbolos).');
+    return;
+  }
+
   const filaNueva = document.createElement('tr');
   const celdaNombre = document.createElement('td');
 
@@ -16,16 +23,15 @@ function agregarNombre(nombreNuevo, idTabla, posicion) {
 
   if (posicion === 'alinicio') {
     tabla.insertBefore(filaNueva, tabla.firstChild);
-    ultimasFilasAgregadas.unshift(filaNueva); // Agregar al principio del array
+    ultimasFilasAgregadas.unshift(filaNueva);
   } else if (posicion === 'alfinal') {
     tabla.appendChild(filaNueva);
-    ultimasFilasAgregadas.push(filaNueva); // Agregar al final del array
+    ultimasFilasAgregadas.push(filaNueva);
   } else if (posicion === 'antesde' || posicion === 'despuesde') {
     const nombreRef = document.getElementById('nombre2').value.trim();
     const filas = tabla.getElementsByTagName('tr');
     let filaReferencia = null;
 
-    // Buscar la fila de referencia según el valor del campo de entrada nombre2
     for (let i = 0; i < filas.length; i++) {
       if (filas[i].textContent.trim() === nombreRef) {
         filaReferencia = filas[i];
@@ -36,14 +42,14 @@ function agregarNombre(nombreNuevo, idTabla, posicion) {
     if (filaReferencia) {
       if (posicion === 'antesde') {
         tabla.insertBefore(filaNueva, filaReferencia);
-        ultimasFilasAgregadas.splice(ultimasFilasAgregadas.indexOf(filaReferencia), 0, filaNueva); // Insertar antes de la fila de referencia
+        ultimasFilasAgregadas.splice(ultimasFilasAgregadas.indexOf(filaReferencia), 0, filaNueva);
       } else {
         if (filaReferencia.nextSibling) {
           tabla.insertBefore(filaNueva, filaReferencia.nextSibling);
-          ultimasFilasAgregadas.splice(ultimasFilasAgregadas.indexOf(filaReferencia.nextSibling), 0, filaNueva); // Insertar después de la fila de referencia
+          ultimasFilasAgregadas.splice(ultimasFilasAgregadas.indexOf(filaReferencia.nextSibling), 0, filaNueva);
         } else {
           tabla.appendChild(filaNueva);
-          ultimasFilasAgregadas.push(filaNueva); // Agregar al final del array si no hay una fila después de la referencia
+          ultimasFilasAgregadas.push(filaNueva);
         }
       }
     } else {
@@ -55,7 +61,7 @@ function agregarNombre(nombreNuevo, idTabla, posicion) {
 function eliminarUltimoAgregado(idTabla) {
   if (ultimasFilasAgregadas.length > 0) {
     const tabla = document.getElementById(idTabla);
-    const ultimaFilaAgregada = ultimasFilasAgregadas.pop(); // Obtener y quitar la última fila del array
+    const ultimaFilaAgregada = ultimasFilasAgregadas.pop();
     tabla.removeChild(ultimaFilaAgregada);
   } else {
     alert('No hay nombres para eliminar');
@@ -87,23 +93,43 @@ function eliminarUltimo(idTabla) {
 
 function eliminarNombre(idTabla) {
   const nombreEliminar = document.getElementById('dato3').value.trim();
+  if (nombreEliminar === '') {
+    alert('Ingresa un nombre para eliminar.');
+    return;
+  }
+
   const tabla = document.getElementById(idTabla);
   const filas = tabla.getElementsByTagName('tr');
 
   for (let i = 0; i < filas.length; i++) {
     if (filas[i].textContent.trim() === nombreEliminar) {
       tabla.removeChild(filas[i]);
-      return; // Termina la función después de eliminar la primera coincidencia
+      return;
     }
   }
   alert('Nombre no encontrado');
 }
 
-
-function cerrarSesion() {
-  window.location.href = 'ruta_index';
+function borrarTodos() {
+  const tabla = document.getElementById('nombres');
+  if (ultimasFilasAgregadas.length > 0) {
+    tabla.innerHTML = '';
+    ultimasFilasAgregadas = [];
+  } else {
+    alert('No hay elementos para borrar');
+  }
 }
 
-function cerrarSesion() {
-  window.location.href = 'ruta_index';
+function vaciarCasillas() {
+  const dato1 = document.getElementById('dato1').value;
+  const nombre2 = document.getElementById('nombre2').value;
+  const dato3 = document.getElementById('dato3').value;
+
+  if (dato1 || nombre2 || dato3) {
+    document.getElementById('dato1').value = '';
+    document.getElementById('nombre2').value = '';
+    document.getElementById('dato3').value = '';
+  } else {
+    alert('Las casillas ya están vacías');
+  }
 }
